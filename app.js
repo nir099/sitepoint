74174f4f12c0543905c6b3380e94412a08ce69aa
nirmal aththanayaka <a.m.s.nirmal.a@gmail.com>
2018-06-28 12:57:01 +0530

    config-passport

---
+++ app.js
@@ -0,0 +1,6 @@
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let passport = require('passport');
--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
-var crypto = require('crypto');
+let crypto = require('crypto');
+let jwt = require('jsonwebtoken');
 
-var userSchema = new mongoose.Schema({
+let userSchema = new mongoose.Schema({
     email: {
         type: String,
         unique: true,
@@ -23,4 +24,16 @@ userSchema.method.setPassword = function( password ) {
 userSchema.method.validPassword = function( password ) {
     let hash = crypto.pbkdf2Sync( password , this.salt , 1000 , 64 , 'sha512').toString('hex');
     return this.hash === hash ;
-}
+};
+
+userSchema.method.genarateJwt = function() {
+    let expiry = new Date();
+    expiry.setDate( expiry.getDate() + 7); // valid for 7 days
+
+    return jwt.sign( {
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt( expiry.getTime() / 1000 ),
+    }, "secret"); // set secret as enviroment variable
+};
